09635bf006cf56570c63a90aefe1bddb34811590
张雄 <a1925449823@gmail.com>
2018-05-29 17:29:11 +0800

    当用户点击屏幕的时候, 为 Vision 植入一个观察对象.

--- Blog-Getting-Started-with-Vision-OC/Blog-Getting-Started-with-Vision-OC/ViewController.m
+++ Blog-Getting-Started-with-Vision-OC/Blog-Getting-Started-with-Vision-OC/ViewController.m
@@ -12,6 +12,7 @@
 
 @interface ViewController () <AVCaptureVideoDataOutputSampleBufferDelegate>
 @property (weak, nonatomic) IBOutlet UIView *cameraView;
+@property (weak, nonatomic) IBOutlet UIView *highlightView;
 @property (nonatomic, strong) AVCaptureVideoPreviewLayer *cameraLayer;
 @property (nonatomic, strong) AVCaptureSession *captureSession;
 @property (nonatomic, strong) VNSequenceRequestHandler *visionSequenceHandle;
@@ -22,6 +23,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    [self setupUI];
     
     // 让 camera 实时获取的视频出现在屏幕上
     [self.cameraView.layer addSublayer:self.cameraLayer];
@@ -35,6 +38,15 @@
     
 }
 
+- (void)setupUI {
+    
+    self.highlightView.layer.borderColor = [[UIColor redColor] CGColor];
+    self.highlightView.layer.borderWidth = 4.0;
+    self.highlightView.backgroundColor = [UIColor clearColor];
+    
+    
+}
+
 - (void)viewDidLayoutSubviews {
     
     [super viewDidLayoutSubviews];
@@ -42,6 +54,23 @@
     // 让 layer 获得正确的大小
     self.cameraLayer.frame = self.cameraView.bounds;
     
+    // 为 观察对象 转换矩阵
+    CGRect originalRect = self.highlightView.frame;
+    CGRect converteRect = [self.cameraLayer metadataOutputRectOfInterestForRect:originalRect];
+    converteRect.origin.y = 1 - converteRect.origin.y;
+    
+    // 设置观察对象
+    VNDetectedObjectObservation *newObservation = [VNDetectedObjectObservation observationWithBoundingBox:converteRect];
+    self.lastObservation = newObservation;
+    
+}
+
+#pragma mark - 点击选框
+- (IBAction)userTapped:(UITapGestureRecognizer *)sender {
+    
+    // 获取点击的中点
+    self.highlightView.frame = CGRectMake(0, 0, 120, 120);
+    self.highlightView.center = [sender locationInView:self.view];
     
 }
 
