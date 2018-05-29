6e4f1d7ec3cbb4b86cb1a42cde4102fc5f0f0b69
张雄 <a1925449823@gmail.com>
2018-05-29 15:11:42 +0800

    创建项目

---
+++ Blog-Getting-Started-with-Vision-OC/Blog-Getting-Started-with-Vision-OC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Blog-Getting-Started-with-Vision-OC
+//
+//  Created by 张雄 on 2018/5/29.
+//  Copyright © 2018年 张雄. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
