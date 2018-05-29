9ec54548ff4940731c842eb6ddc7b1408b8b8af4
张雄 <a1925449823@gmail.com>
2018-05-29 15:30:57 +0800

    获取摄像头的实时影像

--- Blog-Getting-Started-with-Vision-OC/Blog-Getting-Started-with-Vision-OC/ViewController.m
+++ Blog-Getting-Started-with-Vision-OC/Blog-Getting-Started-with-Vision-OC/ViewController.m
@@ -7,23 +7,59 @@
 //
 
 #import "ViewController.h"
+#import <Vision/Vision.h>
+#import <AVFoundation/AVFoundation.h>
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UIView *cameraView;
+@property (nonatomic, strong) AVCaptureVideoPreviewLayer *cameraLayer;
+@property (nonatomic, strong) AVCaptureSession *captureSession;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self.cameraView.layer addSublayer:self.cameraLayer];
+    
+    [self.captureSession startRunning];
+    
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewDidLayoutSubviews {
+    
+    [super viewDidLayoutSubviews];
+    
+    self.cameraLayer.frame = self.cameraView.bounds;
+    
+    
 }
 
+#pragma mark - 懒加载属性
+- (AVCaptureVideoPreviewLayer *)cameraLayer {
+    
+    if (!_cameraLayer) {
+        _cameraLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:self.captureSession];
+    }
+    
+    return _cameraLayer;
+}
+
+- (AVCaptureSession *)captureSession {
+    
+    if (!_captureSession) {
+        _captureSession = [[AVCaptureSession alloc] init];
+        _captureSession.sessionPreset = AVCaptureSessionPresetPhoto;
+        
+        AVCaptureDevice *backCamera = [AVCaptureDevice defaultDeviceWithDeviceType:AVCaptureDeviceTypeBuiltInWideAngleCamera mediaType:AVMediaTypeVideo position:AVCaptureDevicePositionBack];
+        
+        AVCaptureDeviceInput *input = [[AVCaptureDeviceInput alloc] initWithDevice:backCamera error:nil];
+       
+        [_captureSession addInput:input];
+
+    }
+    return _captureSession;
+}
 
 @end
